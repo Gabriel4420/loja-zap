9f98ab3dae76c8b8d54d905fde8755d016780e62
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2025-06-19 22:46:13 -0300

    refactor: :bug: corrigindo link do WhatsApp e ajustando texto do botão

--- src/components/checkout/step-finish.tsx
+++ src/components/checkout/step-finish.tsx
@@ -2,15 +2,26 @@ import { useCheckoutStore } from "@/store/checkout-store";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { generateMessage } from "@/helpers/generate-message";
+import { useEffect } from "react";
 
 export const StepFinish = () => {
   const { user } = useCheckoutStore((state) => state);
 
   const unformattedPhone = user.phone?.replace(/\D/g, ""); // Remove all non-digit characters
   const message = generateMessage();
-  const linkZap = `https://wa.me//+55${
+  const linkZap = `https://wa.me/+55${
     process.env.NEXT_PUBLIC_WHATSAPPNUMBER
   }?text=${encodeURI(message)}`;
+
+  const linkZap2 = `https://wa.me/+55${unformattedPhone}?text=${encodeURI(
+    message
+  )}`;
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.open(linkZap2, "_blank");
+    }
+  }, []);
 
   return (
     <div className="text-center flex flex-col gap-5">
@@ -23,7 +34,7 @@ export const StepFinish = () => {
       </p>
       <Button>
         <Link target="_blank" href={linkZap}>
-          Enviar pata o WhatsApp
+          Enviar para o WhatsApp
         </Link>
       </Button>
     </div>
